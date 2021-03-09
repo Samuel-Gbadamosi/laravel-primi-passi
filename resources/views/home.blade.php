732a2d978f433f8d39ec2f027cf2ab36c24f57fe
samuel <damosisamuel@gmail.com>
2021-03-09 17:04:07 +0100

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+     <?php foreach ($details as $det ): ?>
+       {{$det }}
+     <?php endforeach; ?>
+
+
+ 
+  </body>
+</html>
